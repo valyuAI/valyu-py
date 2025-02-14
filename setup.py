a757a0d189c0085d621aaf2df96989cd8db5952b
Cardio <alexanderngyewshen@gmail.com>
2025-02-14 14:02:48 +0000

    [FEAT] Update api params

--- setup.py
+++ setup.py
@@ -6,32 +6,32 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='valyu',
-    version='1.0.5',
-    author='Valyu',
-    author_email='contact@valyu.network',
-    maintainer='Harvey Yorke',
-    maintainer_email='harvey@valyu.network',
-    description='We handle the data. You handle the AI.',
+    name="valyu",
+    version="1.0.6",
+    author="Valyu",
+    author_email="contact@valyu.network",
+    maintainer="Harvey Yorke",
+    maintainer_email="harvey@valyu.network",
+    description="We handle the data. You handle the AI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://valyu.network",
-    packages=find_packages(exclude=['tests*', '*.downloads*']),
+    packages=find_packages(exclude=["tests*", "*.downloads*"]),
     package_data={
-        'valyu': ['py.typed'],
+        "valyu": ["py.typed"],
     },
     exclude_package_data={
-        '': ['*.pyc', '*.pyo', '*.pyd', '__pycache__', '*.so'],
+        "": ["*.pyc", "*.pyo", "*.pyd", "__pycache__", "*.so"],
     },
     install_requires=[
-        'requests>=2.31.0',
-        'pydantic>=2.5.2',
-        'tqdm>=4.65.0',
+        "requests>=2.31.0",
+        "pydantic>=2.5.2",
+        "tqdm>=4.65.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
