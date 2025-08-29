a3897574e15ce35a2c7c682b6afd5ddad011c17c
Yamamuchi <harveyyorke@proton.me>
2025-08-29 14:45:15 +0100

    [CHORE] Ver. dump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="valyu",
-    version="2.1.0",
+    version="2.2.0",
     author="Valyu",
     author_email="contact@valyu.network",
     maintainer="Harvey Yorke",
